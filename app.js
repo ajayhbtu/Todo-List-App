47f7e3059b21601a12607449df6813cd5eb21e6c
ajayhbtu <ajaygupta.hbtu@gmail.com>
2020-10-03 11:33:13 +0530

    First commit todoList App

---
+++ app.js
@@ -0,0 +1,188 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const date = require(__dirname + "/date.js");
+const secret = require(__dirname + "/secrets.js");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+const usrname = secret.authCred.username || process.env.USERNAME;
+const pswrd = secret.authCred.password || process.env.PASSWORD;
+
+mongoose.connect("mongodb+srv://" + usrname + ":" + pswrd +"@cluster0.6lxda.mongodb.net/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your Todo list!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+
+const defaultItems = [item1, item2, item3];
+
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+}
+
+const List = mongoose.model("List", listSchema);
+
+
+
+app.get("/", function(req, res) {
+
+  const day = date.getDate();
+
+  Item.find({}, function(err, foundItems) {
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: day,
+        newListItems: foundItems
+      });
+    }
+  });
+
+});
+
+
+app.get("/cat/:customListName", function(req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save(function(err, result) {
+          res.redirect("/cat/" + customListName);
+        });
+      } else {
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
+      }
+    } else {
+      console.log(err);
+    }
+  });
+
+});
+
+
+
+app.post("/", function(req, res) {
+
+  const itemName = req.body.newItem;
+  const listName = req.body.listName;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/");
+
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      if (!err) {
+        foundList.items.push(item);
+        foundList.save();
+      } else {
+        console.log(err);
+      }
+    });
+    res.redirect("/cat/" + listName);
+  }
+
+});
+
+
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndDelete(checkedItemId, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully Deleted checked item from today list.");
+        res.redirect("/");
+      }
+    });
+
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: checkedItemId
+        }
+      }
+    }, function(err, foundList) {
+      if (!err) {
+        console.log("Successfully Deleted checked item from " + listName + " list.");
+        res.redirect("/cat/" + listName);
+      } else {
+        console.log(err);
+      }
+    });
+  }
+
+});
+
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
